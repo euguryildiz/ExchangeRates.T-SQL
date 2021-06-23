459487308d4c8523a86666826a76a84781dc0c91
Uğur Yıldız <yldz2534@gmail.com>
2021-06-24 00:13:21 +0300

    first commit

--- ExchangeRates.sql
+++ ExchangeRates.sql
@@ -8,15 +8,15 @@ Parameter(s):	@Date					:	The date of the exchange rate you want to receive
 				@ErrorStatus			:	If 0 is given, it gives an error if there is no data. If 1 is given, empty record is returned if there is no data.
 Usage		:	DECLARE @CurrencyDate date = GETDATE()
 				EXEC dbo.sp_ExchangeRates @Date = @CurrencyDate, @ErrorStatus = 0
-Dependencies:	Ole Automation:
-					sp_configure 'show advanced options', 1;
-					GO
-					RECONFIGURE;
-					GO
-					sp_configure 'Ole Automation Procedures', 1;
-					GO
-					RECONFIGURE;
-					GO
+Dependencies:	
+				sp_configure 'show advanced options', 1;
+				GO
+				RECONFIGURE;
+				GO
+				sp_configure 'Ole Automation Procedures', 1;
+				GO
+				RECONFIGURE;
+				GO
 */
 
 CREATE PROCEDURE sp_ExchangeRates 
